fcf244af01cedd6989da7ec672fb8dde65376035
byoso <emailvinz@gmail.com>
2025-08-09 15:07:37 +0200

    DEBUG mode to save ElevenLabs credits

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import os
+import json
 
 import requests
 
@@ -8,38 +9,57 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+DEBUG = os.environ.get("DEBUG", "1") == "1"
 ELEVENLABS_API_KEY = os.environ.get("ELEVENLABS_API_KEY", "no_environment_set")
 
 characters = [
-    {"name": "James", "voice_id": "EkK5I93UQWFDigLMpZcX"},
-    {"name": "Arabella", "voice_id": "Z3R5wn05IrDiVCyEkUrK"},
-    {"name": "Blondie", "voice_id": "exsUS4vynmxd379XN4yO"},
-    {"name": "Kuon", "voice_id": "B8gJV1IhpuegLxdpXFOE"},
+    {"name": "James", "voice_id": "EkK5I93UQWFDigLMpZcX","dialog": {
+        "title": "try",
+        "text": "Let's try !"
+    }},
+    {"name": "Arabella", "voice_id": "Z3R5wn05IrDiVCyEkUrK","dialog": {
+        "title": "try",
+        "text": "Let's try !"
+    }},
+    {"name": "Blondie", "voice_id": "exsUS4vynmxd379XN4yO","dialog": {
+        "title": "try",
+        "text": "Let's try !"
+    }},
+    {"name": "Kuon", "voice_id": "B8gJV1IhpuegLxdpXFOE","dialog": {
+        "title": "try",
+        "text": "Let's try !"
+    }},
 ]
 
 
 def convert_text_to_speech(char):
     # Create speech (POST /v1/text-to-speech/:voice_id)
-    response = requests.post(
-        f"https://api.elevenlabs.io/v1/text-to-speech/{char['voice_id']}?output_format=mp3_44100_128",
-        # f"https://api.elevenlabs.io/v1/text-to-speech/JBFqnCBsd6RMkjVDRZzb?output_format=mp3_44100_128",
-    headers={
-        "xi-api-key": f"{ELEVENLABS_API_KEY}"
-    },
-    json={
-        "text": "The first move is what sets everything in motion.",
-        "model_id": "eleven_multilingual_v2"
-    },
-    )
-    if response.status_code != 200:
-        print(f"Something when wrong with {char['name']}")
+    if DEBUG:
+        file_path = f"debug-{char['name']}.txt"
+        with open(file_path, "wb") as f:
+            f.write(json.dumps(char["dialog"]).encode())
 
-    print(char['name'], response.status_code)
-    # print(response.json())
-        # Sauvegarder le MP3
-    file_path = f"{char['name']}.mp3"
-    with open(file_path, "wb") as f:
-        f.write(response.content)
+    if not DEBUG:
+        response = requests.post(
+            f"https://api.elevenlabs.io/v1/text-to-speech/{char['voice_id']}?output_format=mp3_44100_128",
+            # f"https://api.elevenlabs.io/v1/text-to-speech/JBFqnCBsd6RMkjVDRZzb?output_format=mp3_44100_128",
+        headers={
+            "xi-api-key": f"{ELEVENLABS_API_KEY}"
+        },
+        json={
+            "text": "The first move is what sets everything in motion.",
+            "model_id": "eleven_multilingual_v2"
+        },
+        )
+        if response.status_code != 200:
+            print(f"Something when wrong with {char['name']}")
+
+        print(char['name'], response.status_code)
+
+        # Save MP3
+        file_path = f"{char['name']}.mp3"
+        with open(file_path, "wb") as f:
+            f.write(response.content)
 
 
 
